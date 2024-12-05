d5acdd3f5b6218b9edf356c573ecee01eaba43a1
juliaoliveira1280 <juliabispo983@gmail.com>
2024-12-05 11:26:02 -0300

    Add files via upload

---
+++ aula 2/aula02.py
@@ -0,0 +1,11 @@
+print('hoje o dia esta lindo ')
+print('vou comecar a estudar ingles e espanhol')
+nome_usario ='julia bispo de oliveira'# tipo 
+idade_usuario = 21 # tipo int =integer
+altura_usuario=1.77#tipo => float = flutuante
+maior_idade = True # tipo -> bool = boolean
+print('seja bem vindo ', nome_usario)
+print('sua idade é', idade_usuario, 'anos')
+print('você tem', altura_usuario, 'metros.')
+print('voce e maior'maior_idade)
+print('menor de idade )
